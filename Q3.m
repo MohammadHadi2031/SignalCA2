516a533d06be74e6255cf87f116c28961c72bf9a
MohammadHadiAttarieh <mohammadhadi2031@gmail.com>
2018-05-22 19:03:16 +0430

    removing eco with Z transform

--- Q3.m
+++ Q3.m
@@ -2,7 +2,8 @@
 [S, Fs] = audioread('sound.wav');
 msgbox({sprintf('Fs = %d',Fs), sprintf('Length = %d', numel(S))});
 %% sampling
-sampleS = S(1:4000:end);
+sampleDiff = round(Fs/4000);
+sampleS = S(1:sampleDiff:end);
 sampleSplayer = audioplayer(sampleS, Fs);
 play(sampleSplayer); % no sound
 %% new FS
@@ -16,7 +17,7 @@ obw(sampleS, 4000);
 %% reconst
 t = 0:1/Fs:(numel(S)-1)/Fs;
 reS = reconst(sampleS, t, 1/4000);
-
+%reS =  arrayfun(reSfun, t);
 
 
 %% play recontructed sound
