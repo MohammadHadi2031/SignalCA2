7905f21691cf70565822a4ce0dc4f8ea1ff956cb
MohammadHadiAttarieh <mohammadhadi2031@gmail.com>
2018-05-24 18:19:23 +0430

    reconstructed 4KHz sound

--- Q3.m
+++ Q3.m
@@ -16,8 +16,10 @@ subplot(2, 1, 2);
 obw(sampleS, 4000);
 %% reconst
   %% initialize
-    t = gpuArray(0:1/Fs:(numel(S)-1)/Fs);
-    sampleSgpu = gpuArray(sampleS);
+    %t = gpuArray(0:1/Fs:(numel(S)-1)/Fs);
+    %sampleSgpu = gpuArray(sampleS);
+     t = 0:1/Fs:(numel(S)-1)/Fs;
+    sampleSgpu = sampleS;
   %% calculate
     tic
     reS = reconst(sampleS, t, 1/4000);
@@ -25,3 +27,21 @@ obw(sampleS, 4000);
 %% play recontructed sound
 reSplayer = audioplayer(reS, Fs);
 play(reSplayer);
+%% compare by fft
+f = Fs*(0:L/2)/L;
+Y = fft(S); 
+Y = Y(1:L/2+1);
+Y = abs(Y);
+
+Y2 = fft(reS);
+Y2 = Y2(1:L/2+1);
+Y2 = abs(Y2);
+
+
+subplot(2, 1, 1);
+plot(f, Y);
+title('fft of original sound');
+
+subplot(2, 1, 2);
+plot(f, Y2);
+title('fft of reconstructed sound');
